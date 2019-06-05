cd2d5e33fbfc14a3a0c94d02f08ed94bcfaa108e
Matthew Griffith <mdg.griffith@gmail.com>
2019-06-04 20:01:46 -0400

    gzip js

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 elm make src/Main.elm --output=assets/elm.js --optimize
 google-closure-compiler --js=assets/elm.js --js_output_file=assets/elm.min.js
+gzip assets/elm.min.js
 cp -a assets/. public/
-mv public/elm.min.js public/elm.js
+mv public/elm.min.js.gz public/elm.js
